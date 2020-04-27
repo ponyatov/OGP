476e8562b91aff434aafa45541a47eb315e55fed
Dmitry Ponyatov <dponyatov@gmail.com>
2020-04-28 03:17:20 +0400

    + wsgi
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- pythonanywhere/kbase_pythonanywhere_com_wsgi.py
+++ pythonanywhere/kbase_pythonanywhere_com_wsgi.py
@@ -13,7 +13,6 @@ if project_home not in sys.path:
     sys.path = [project_home] + sys.path
 
 # import flask app but need to call it "application" for WSGI to work
-from metaL import app as application  # noqa
 
 from metaL import *
 with open('metaL.ini') as src:
